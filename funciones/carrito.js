1560716468ec571748961c75ba7759936c6fa1e3
vsol <vsol.sil@gmail.com>
2021-08-25 02:39:25 -0300

    Carrito con renderizado en index
    posicionamiento del carrito al hacer click en el icono
    
    paso siguiente: renderizar los productos añadidos

--- funciones/carrito.js
+++ funciones/carrito.js
@@ -1,1 +1,14 @@
-window.onload= visualizarCarrito(); //--> load.js
+//window.onload = visualizarCarrito(); //--> load.js
+
+const viewCart = () => {
+    const cartContainer = document.querySelector(".visualizarCarrito")
+    cartContainer.classList.toggle("display-block")
+    cartContainer.classList.toggle("view-cart")
+
+    const bodyGrid = document.querySelector("#productsBody")
+    bodyGrid.classList.toggle("productsBody")
+  //console.log(cartContainer);
+};
+
+const cartBtn = document.querySelector("#cart-btn");
+cartBtn.addEventListener("click", viewCart);
